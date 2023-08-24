3082ff3ad3d52090031e370d21a5a3e235e9b821
Kun Alfin Hidayat <kahid.na@gmail.com>
2023-08-24 13:52:03 +0700

    Update get-pretty-node-label.sh
    update script

--- get-pretty-node-label.sh
+++ get-pretty-node-label.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker node ls -q | xargs docker node inspect -f '{{ .ID }} [{{ .Description.Hostname }}]: {{ .Spec.Labels }}' | cut -d" " -f1 --complement| sed 's/:/=/g'| sed 's/\[//g' |sed 's/\]/ /g'
+docker node ls -q | xargs docker node inspect -f '{{ .ID }} [{{ .Description.Hostname }}]: {{ .Spec.Labels }}' | cut -d" " -f1 --complement| sed 's/:/=/g'| sed 's/= map\[/: /g' |sed 's/\[//g' |sed 's/\]/ /g'
